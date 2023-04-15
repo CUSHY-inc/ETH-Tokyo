fcbeced3fb4552e1e8a2a73b00f6d2af83795825
ak0007-code <asahi0107wave@gmail.com>
2023-04-15 18:59:43 +0900

    アイコンの大きさを変更

--- src/components/chatList/ChatList.tsx
+++ src/components/chatList/ChatList.tsx
@@ -3,7 +3,7 @@ import Avatar from "../common/Avatar";
 const ChatList = ({ image='', name='', explain='', date='' }) => {
     return (
         <div className="flex">
-            <Avatar image={image} ringOffset='ring-offset-0' w='w-10' />
+            <Avatar image={image} ringOffset='ring-offset-0' w='w-10' h='h-10' />
             <div className="ml-4 text-lg ">
                 <p className="text-base">{name}</p>
                 <div className="text-gray-400 text-base flex">
