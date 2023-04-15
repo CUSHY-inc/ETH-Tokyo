6acc616cefd37da12678b0ed1d3755796749dc72
Ryo Uemura <ruemura3.contact@gmail.com>
2023-04-16 04:01:57 +0900

    debug

--- src/features/chat/huddle01.ts
+++ src/features/chat/huddle01.ts
@@ -1,18 +1,22 @@
 const postData = async () => {
-  const response = await fetch('https://iriko.testing.huddle01.com/api/v1/create-iframe-room', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': 'VwTZ4AGTxme9snANex9tep3NwvVMGfYd'
-    },
-    body: JSON.stringify({ title: 'NetworX' })
-  });
-  if (response.ok) {
-    const responseData = await response.json();
-    console.log(responseData.data.meetingLink);
-    window.open(responseData.data.meetingLink, '_blank')
-  } else {
-    console.error('Failed to fetch data')
+  try {
+    const response = await fetch('https://iriko.testing.huddle01.com/api/v1/create-iframe-room', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': 'VwTZ4AGTxme9snANex9tep3NwvVMGfYd'
+      },
+      body: JSON.stringify({ title: 'NetworX' })
+    });
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log(responseData.data.meetingLink);
+      window.open(responseData.data.meetingLink, '_blank')
+    } else {
+      console.error('Failed to fetch data')
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
 
